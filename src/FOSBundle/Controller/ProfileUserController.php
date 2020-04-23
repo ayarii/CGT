1f6a3c3f298b42c6c4dab4b82582dacbb1647bd7
louayYahyaoui <louay.yahyaoui@esprit.tn>
2020-04-23 19:55:40 +0100

    Merge branch 'master' of C:\wamp64\www\CGT1 with conflicts.

--- src/FOSBundle/Controller/ProfileUserController.php
+++ src/FOSBundle/Controller/ProfileUserController.php
@@ -11,15 +11,7 @@ use FOSBundle\Entity\Amitie;
 class ProfileUserController  extends Controller
 {
 
-    public function afficherProfileAction(Request $request,$id){
-        $em=$this->getDoctrine()->getManager();
-        $Profile=$em->getRepository("FOSBundle:User")->findOneBy(array('id'=>$id));
-        $Amie=$em->getRepository("FOSBundle:Amitie")->findAll();
-        return $this->render('@FOS/Default/profile.html.twig',
-            array('Profile'=>$Profile,
-                'Amie'=>$Amie,
-            ));
-    }
+
 
     public function sendRequestAction(Request $request,$id)
     {
@@ -27,18 +19,16 @@ class ProfileUserController  extends Controller
         $user = $this->getUser();
         $em = $this->getDoctrine()->getManager();
         $reciver=$em->getRepository('FOSBundle:User')->findOneBy(array('id'=>$id));
-        $Amitie->setIduser1($user);
-        $Amitie->setIduser2($reciver);
+        $Amitie->setIdme($user);
+        $Amitie->setIduser($reciver);
         $Amitie->setSenderid($user);
         $Amitie->setEtat(0);
         $Amitie->setBlockid(0);
 
         $em->persist($Amitie);
         $em->flush();
-        return $this->redirectToRoute('profile',array(
-            'id'=>$id,
-            )
-        );
+
+        return $this->redirectToRoute('visit_profile',array('id_user'=>$id));
 
     }
 
@@ -67,23 +57,12 @@ class ProfileUserController  extends Controller
     public function afficherRequestAction(){
         $em=$this->getDoctrine()->getManager();
         $user=$this->getUser();
-        $Invitations=$em->getRepository("FOSBundle:Amitie")->findBy(array('iduser2'=>$user->getId(),'etat'=>0));
+        $Invitations=$em->getRepository("FOSBundle:Amitie")->findBy(array('iduser'=>$user->getId(),'etat'=>0));
         return $this->render('@FOS/Default/invitations.html.twig',
             array('Invitations'=>$Invitations));
 
     }
 
-    public function afficherFriendsAction(){
-        $em=$this->getDoctrine()->getManager();
-        $user=$this->getUser();
-        $Friends1=$em->getRepository("FOSBundle:Amitie")->findBy(array('iduser1'=>$user->getId(),'etat'=>1));
-        $Friends2=$em->getRepository("FOSBundle:Amitie")->findBy(array('iduser2'=>$user->getId(),'etat'=>1));
 
-        return $this->render('@FOS/Default/friends.html.twig',
-            array('Friends1'=>$Friends1,
-                'Friends2'=>$Friends2,
-                ));
-
-    }
 
 }
